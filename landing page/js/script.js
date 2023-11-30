e36abd96b88ea122e606a727bf5be57b92ed2c40
RSA <ravils465@gmail.com>
2023-11-30 16:21:46 +0300

    landing-page

---
+++ landing page/js/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.form')
+const input= document.querySelector('.form__input')
+
+input.addEventListener('focus', function(){
+    form.classList.add('form--active')
+})
+input.addEventListener('blur', function(){
+    form.classList.remove('form--active')
+})
